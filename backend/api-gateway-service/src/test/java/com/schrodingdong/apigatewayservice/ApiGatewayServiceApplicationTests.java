6d1b5e72dfc501cb9d4d41fa84d092d5151f32f3
Schrodingdong <91998001+Schrodingdong@users.noreply.github.com>
2023-07-17 11:51:29 +0100

    [BACKEND] Add Api-Gateway and Discovery-Service

---
+++ backend/api-gateway-service/src/test/java/com/schrodingdong/apigatewayservice/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schrodingdong.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
