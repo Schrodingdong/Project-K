781764a0baf403f5c29949eed5f562a132b179fa
Schrodingdong <91998001+Schrodingdong@users.noreply.github.com>
2023-07-22 15:04:21 +0100

    [QUOTE-MANAG-SERV] Init Service

---
+++ backend/quote-manager-service/src/main/java/com/schrodingdong/quotemanagerservice/QuoteManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.schrodingdong.quotemanagerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteManagerServiceApplication.class, args);
+	}
+
+}
---
+++ backend/quote-manager-service/src/test/java/com/schrodingdong/quotemanagerservice/QuoteManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schrodingdong.quotemanagerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
