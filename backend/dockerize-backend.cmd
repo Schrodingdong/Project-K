8f36910cd7e117f14c5bc1ecc5dcc9f624994195
Schrodingdong <91998001+Schrodingdong@users.noreply.github.com>
2023-08-25 00:54:25 +0100

    [DEVOPS] Dockerize Backend

---
+++ backend/dockerize-backend.cmd
@@ -0,0 +1,5 @@
+docker build -t project-k-api-gateway ./api-gateway-service
+docker build -t project-k-authentication ./authentication-service
+docker build -t project-k-discovery ./discovery-service
+docker build -t project-k-quote-manager ./quote-manager-service
+docker build -t project-k-user-manager ./user-manager-service
